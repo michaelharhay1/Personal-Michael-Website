ba45c383a5e6059e09128c0e7e7fa285fdf0ce36
mharhay213 <151762557+mharhay213@users.noreply.github.com>
2024-08-05 15:57:50 -0400

    Updated background, still need to tweak mouse interaction as well as resizability, then layer text over it.

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ var w = window.innerWidth,
     h = window.innerHeight,
     canvas = document.getElementById('background'),
     ctx = canvas.getContext('2d'),
-    rate = 40,
-    arc = 60,
+    rate = 30,
+    numParts = 50,
     time,
     count,
     size = 7,
@@ -38,46 +38,52 @@ var w = window.innerWidth,
 
 var mouse = {x: 0, y: 0};
 
-canvas.setAttribute('width',w);
-canvas.setAttribute('height',h);
+canvas.setAttribute('width', w);
+canvas.setAttribute('height', h);
 create();
 particles();
 
+// Initialize particle array //
 function create() {
     time = 0;
     count = 0;
 
-    for(var i = 0; i < arc; i++) {
+    for (var i = 0; i < numParts; i++) {
         parts[i] = {
             x: Math.ceil(Math.random() * w),
             y: Math.ceil(Math.random() * h),
             toX: Math.random() * 5 - 1,
             toY: Math.random() * 2 - 1,
             c: colors[Math.floor(Math.random() * colors.length)],
-            size: Math.random() * size
+            size: (Math.random() / 2 + 0.25) * size
         }
     }
 }
 
+// Customize particle movement //
 function particles() {
     ctx.clearRect(0, 0, w, h);
     canvas.addEventListener('mousemove', MouseMove, false);
     
-    for (var i = 0; i < arc; i++) {
+    for (var i = 0; i < numParts; i++) {
         var li = parts[i];
         var distanceFactor = DistanceBetween(mouse, parts[i]);
         var distanceFactor = Math.max(Math.min(15 - (distanceFactor / 10), 10), 1);
+        
         ctx.beginPath();
         ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
         ctx.fillStyle = li.c;
-        ctx.strokeStyle=li.c;
-        
+        ctx.strokeStyle = li.c;
+        ctx.fill()
+
+        /*
         if (i % 2 == 0) {
             ctx.stroke();
         }
         else {
             ctx.fill();
         }
+        */
         
         li.x = li.x + li.toX * (time * 0.05);
         li.y = li.y + li.toY * (time * 0.05);
@@ -103,16 +109,15 @@ function particles() {
     setTimeout(particles, 1000 / rate);
 }
 
+// Respond to mouse movement //
 function MouseMove(e) {
    mouse.x = e.layerX;
    mouse.y = e.layerY;
-
-   //context.fillRect(e.layerX, e.layerY, 5, 5);
-   //Draw( e.layerX, e.layerY );
 }
 
-function DistanceBetween(p1,p2) {
-   var dx = p2.x-p1.x;
-   var dy = p2.y-p1.y;
-   return Math.sqrt(dx*dx + dy*dy);
+// Get distance between two points //
+function DistanceBetween(p1, p2) {
+   var dx = p2.x - p1.x;
+   var dy = p2.y - p1.y;
+   return Math.sqrt(dx * dx + dy * dy);
 }
