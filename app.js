5861c174141f916cfbdf6a1760b0c933d0b03594
mharhay213 <151762557+mharhay213@users.noreply.github.com>
2024-08-05 14:30:05 -0400

    Cleaned up mess from trying to fix button re-animation on page load - still not fixed. Also added background to home page - needs cleaning up and tweaking.

--- app.js
+++ app.js
@@ -7,31 +7,8 @@
     Functionality: Contains JS scripts for personal website
 */
 
-// --- Button Animation Control --- //
-document.addEventListener("DOMContentLoaded", function() {
-    // Retrieve the id of the active button from session storage
-    var activeButtonId = sessionStorage.getItem("activeButtonId");
-    
-    // If there's an active button id stored, set the active class to the corresponding button
-    if (activeButtonId) {
-        var activeButton = document.getElementById(activeButtonId);
-        if (activeButton) {
-            activeButton.classList.add("active");
-        }
-    }
-
-    // Add click event listeners to the buttons to store the active button id in session storage
-    var buttons = document.querySelectorAll("nav a");
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            sessionStorage.setItem("activeButtonId", button.id);
-        });
-    });
-});
-
-
 // --- Typewriter Effect --- //
-var app = document.getElementById("paragraph");
+var app = document.getElementById('paragraph');
 
 var Typewriter = new Typewriter(app, {
     loop: true,
@@ -40,6 +17,102 @@ var Typewriter = new Typewriter(app, {
 
 Typewriter
     .pauseFor(1000)
-    .typeString("A driven, goal-oriented computer engineering student with a love for all things software, and a growing interest in artificial intelligence and data. A diligent, creative problem-solver with a dedication to excellence and a love for learning and self-improvement. A confident, accountable leader who brings out the best in others.")
+    .typeString('A driven, goal-oriented computer engineering student with a love for all things software, and a growing interest in artificial intelligence and data. A diligent, creative problem-solver with a dedication to excellence and a love for learning and self-improvement. A confident, accountable leader who brings out the best in others.')
     .pauseFor(2000)
     .start()
+
+
+// --- Particles Background Effect --- //
+var w = window.innerWidth,
+    h = window.innerHeight,
+    canvas = document.getElementById('background'),
+    ctx = canvas.getContext('2d'),
+    rate = 40,
+    arc = 60,
+    time,
+    count,
+    size = 7,
+    speed = 20,
+    parts = new Array,
+    colors = ['#ffffff','#E0E0E0','#757575','#64B5F6','#6B99BE'];
+
+var mouse = {x: 0, y: 0};
+
+canvas.setAttribute('width',w);
+canvas.setAttribute('height',h);
+create();
+particles();
+
+function create() {
+    time = 0;
+    count = 0;
+
+    for(var i = 0; i < arc; i++) {
+        parts[i] = {
+            x: Math.ceil(Math.random() * w),
+            y: Math.ceil(Math.random() * h),
+            toX: Math.random() * 5 - 1,
+            toY: Math.random() * 2 - 1,
+            c: colors[Math.floor(Math.random() * colors.length)],
+            size: Math.random() * size
+        }
+    }
+}
+
+function particles() {
+    ctx.clearRect(0, 0, w, h);
+    canvas.addEventListener('mousemove', MouseMove, false);
+    
+    for (var i = 0; i < arc; i++) {
+        var li = parts[i];
+        var distanceFactor = DistanceBetween(mouse, parts[i]);
+        var distanceFactor = Math.max(Math.min(15 - (distanceFactor / 10), 10), 1);
+        ctx.beginPath();
+        ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
+        ctx.fillStyle = li.c;
+        ctx.strokeStyle=li.c;
+        
+        if (i % 2 == 0) {
+            ctx.stroke();
+        }
+        else {
+            ctx.fill();
+        }
+        
+        li.x = li.x + li.toX * (time * 0.05);
+        li.y = li.y + li.toY * (time * 0.05);
+        
+        if (li.x > w){
+            li.x = 0; 
+        }
+        if (li.y > h) {
+            li.y = 0; 
+        }
+        if (li.x < 0) {
+            li.x = w; 
+        }
+        if (li.y < 0) {
+            li.y = h; 
+        }
+    }
+
+    if (time < speed) {
+        time++;
+    }
+
+    setTimeout(particles, 1000 / rate);
+}
+
+function MouseMove(e) {
+   mouse.x = e.layerX;
+   mouse.y = e.layerY;
+
+   //context.fillRect(e.layerX, e.layerY, 5, 5);
+   //Draw( e.layerX, e.layerY );
+}
+
+function DistanceBetween(p1,p2) {
+   var dx = p2.x-p1.x;
+   var dy = p2.y-p1.y;
+   return Math.sqrt(dx*dx + dy*dy);
+}
